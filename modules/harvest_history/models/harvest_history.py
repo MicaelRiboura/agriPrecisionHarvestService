d3e15f9ecf1306985767f0d8820f93f0e015652c
MicaelRiboura <micaelribouradossantos@gmail.com>
2024-07-01 07:48:14 -0300

    build(harvest_history):  adjust model

--- modules/harvest_history/models/harvest_history.py
+++ modules/harvest_history/models/harvest_history.py
@@ -1,20 +1,25 @@
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Date
 from modules.shared.config.model.base import Base
 from modules.shared.config.model.serializer import Serializer
+from datetime import datetime
 
 class HarvestHistory(Base):
     __tablename__ = 'harvest_history'
 
     id = Column(Integer, primary_key=True)
-    date = Column(Date)
+    date = Column(Date, unique=True)
     total_production = Column(Integer)
     field = Column(Integer, ForeignKey('field.id'), nullable=False)
     user = Column(String(500))
 
-    def __init__(self, date: str, total_production: float):
-        self.date = date
+    def __init__(self, date: str, total_production: float, field: int, user: str):
+        self.date = datetime.strptime(date, '%Y-%m-%d')
         self.total_production = total_production
+        self.field = field
+        self.user = user
 
     def serialize(self):
-        harvest_history = Serializer.serialize(self)
+        harvest_history_formated = self
+        harvest_history_formated.date = datetime.strftime(self.date, '%Y-%m-%d')
+        harvest_history = Serializer.serialize(harvest_history_formated)
         return harvest_history
