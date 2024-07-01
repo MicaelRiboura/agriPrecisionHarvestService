c1e8e1676ccb71ef5b24b0a6786947cc1cc156f8
MicaelRiboura <micaelribouradossantos@gmail.com>
2024-07-01 08:21:40 -0300

    refactor: change response schema in delete routes to a shared schema

--- modules/field/schemas/__init__.py
+++ modules/field/schemas/__init__.py
@@ -4,4 +4,3 @@ from .list_fields_by_user_schema import ListFieldsByUserQuerySchema
 from .create_field_schema import CreateFieldSchema
 from .field_by_id_query_schema import FieldByIdQuerySchema
 from .edit_field_schema import EditFieldSchema
-from .delete_response_schema import DeleteFieldSchema
--- modules/field/schemas/delete_response_schema.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class DeleteFieldSchema(BaseModel):
-    """ Define como uma mensagem de talhão removido com sucesso é representada.
-    """
-    message: str
