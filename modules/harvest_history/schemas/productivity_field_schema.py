9ff3d4651adeb91969972b77002e4f8e819fa0ca
MicaelRiboura <micaelribouradossantos@gmail.com>
2024-07-08 15:25:03 -0300

    fix(harvest-history): adjust dao and schema of map productivity

--- modules/harvest_history/schemas/productivity_field_schema.py
+++ modules/harvest_history/schemas/productivity_field_schema.py
@@ -4,4 +4,5 @@ class ProductivityFieldSchema(BaseModel):
     """Define como um registro de mapa de produtividade dos talhões é representado.
     """
     field:int = 1
+    planting:str = "Soja"
     average: int = 30
